f02d86889d0c72b5451da806387ec39c33500721
seansustainable <seansustainable@gmail.com>
2021-04-12 11:35:30 +0900

    add wordrelay

--- wordRelay/WordRelay.jsx
+++ wordRelay/WordRelay.jsx
@@ -1,14 +1,92 @@
+const { useState, useRef } = require('react');
 const React = require('react');
 const { Component } = React;
 
-class WordRelay extends Component {
-    state = {
-        text: 'halo',
+/* Class */
+// class WordRelay extends Component {
+//     state = {
+//         word: '고구마',
+//         value: '',
+//         result: '',
+//     }
+
+//     onChangeInput = (e) => {
+//         this.setState({
+//             value: e.target.value,
+//         })
+//     }
+
+//     onSubmitForm = (e) => {
+//         e.preventDefault();
+//         if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+//             this.setState({
+//                 result: 'PASS!',
+//                 word: this.state.value,
+//                 value: '',
+//             })
+//         } else {
+//             this.setState({
+//                 result: 'FAIL!',
+//                 value: '',
+//             })
+//         }
+//         this.input.focus();
+//     }
+
+//     input;
+//     refInput = (c) => {
+//         this.input = c;
+//     }
+
+//     render() {
+//         return (
+//             <>
+//                 <div>{this.state.word}</div>
+//                 <form onSubmit={this.onSubmitForm}>
+//                     <input ref={this.refInput} value={this.state.value} onChange={this.onChangeInput} />
+//                     <button>입력</button>
+//                 </form>
+//                 <div>{this.state.result}</div>
+//             </>
+//         );
+//     }
+// }
+
+/* Hooks */
+const WordRelay = () => {
+    const [word, setWord] = useState('고구마');
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState('');
+    const refInput = useRef(null);
+
+    const onChangeInput = (e) => {
+        setValue(e.target.value)
     }
 
-    render() {
-        return <div>{this.state.text}</div>;
+    const onSubmitForm = (e) => {
+        e.preventDefault();
+        if (word[word.length - 1] === value[0]) {
+            setResult('PASS!');
+            setWord(value);
+            setValue('');
+        } else {
+            setResult('FAIL!');
+            setValue('');
+        }
+        refInput.current.focus();
     }
+
+    return (
+        <>
+            <div>{word}</div>
+            <form onSubmit={onSubmitForm}>
+                <input ref={refInput} value={value} onChange={onChangeInput} />
+                <button>입력</button>
+            </form>
+            <div>{result}</div>
+        </>
+    );
+
 }
 
 module.exports = WordRelay;
--- wordRelay/webpack.config.js
+++ wordRelay/webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const webpack = require('webpack');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
     name: 'wordrelay-setting',
     mode: 'development', // 실서비스: production
-    devtool: 'eval', // 빠르게
+    devtool: 'eval', // 빠르게, 실서비스: hidden-source-map
     resolve: {
         extensions: ['.js', '.jsx']
     },
@@ -12,20 +14,36 @@ module.exports = {
         app: ['./client'],
     },
 
-    module: { // 모듈을 적용한다.
+    module: { // 모듈(Loaders)을 적용한다. 
         rules: [{
             test: /\.jsx?/, // 정규표현식
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
-                plugins: ['@babel/plugin-proposal-class-properties'],
+                presets: [['@babel/preset-env', {
+                    targets: {
+                        browsers: ['> 5% in KR'], // browserslist
+                    },
+                    debug: true,
+                }], '@babel/preset-react'],
+                plugins: ['@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel',
+                ],
             }
         }]
     },
 
+    plugins: [
+        new webpack.LoaderOptionsPlugin({ debug: true }),
+        new RefreshWebpackPlugin(),
+    ],
+
     output: { // 출력
         path: path.join(__dirname, 'dist'),
-        filename: 'app.js'
+        filename: 'app.js',
+        publicPath: '/dist/', // app.use('/dist', express.static(__dirname, 'dist'))
+    },
+    devServer: {
+        publicPath: '/dist/', // 가상 경로
+        hot: true,
     }
-
 }
