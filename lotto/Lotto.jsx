dbda4b0b42be96457f0289010670a6f83da7d78a
seansustainable <seansustainable@gmail.com>
2021-04-14 10:16:59 +0900

    add minesearch

--- lotto/Lotto.jsx
+++ lotto/Lotto.jsx
@@ -14,128 +14,128 @@ function getWinNumbers() {
 }
 
 /* Hooks */
-const Lotto = () => {
-    const lottoNumbers = useMemo(() => getWinNumbers(), []);
-    const [winNumbers, setWinNumbers] = useState(lottoNumbers);
-    const [winBalls, setWinBalls] = useState([]);
-    const [bonus, setBonus] = useState(null);
-    const [redo, setRedo] = useState(false);
-    const timeOuts = useRef([]);
+// const Lotto = () => {
+//     const lottoNumbers = useMemo(() => getWinNumbers(), []);
+//     const [winNumbers, setWinNumbers] = useState(lottoNumbers);
+//     const [winBalls, setWinBalls] = useState([]);
+//     const [bonus, setBonus] = useState(null);
+//     const [redo, setRedo] = useState(false);
+//     const timeOuts = useRef([]);
 
-    useEffect(() => {
-        runTimeouts();
-        return () => {
-            timeOuts.current.forEach((v) => {
-                clearTimeout(v);
-            });
-        }
-    }, [timeOuts.current]); // 배열에 요소가 있으면 componentDidMount와 componentDidUpdate를 둘 다 수행한다.
-    // timeOuts.current가 바뀌는 시점은 새 배열을 넣어주는 시점 -> onClickRedo
+//     useEffect(() => {
+//         runTimeouts();
+//         return () => {
+//             timeOuts.current.forEach((v) => {
+//                 clearTimeout(v);
+//             });
+//         }
+//     }, [timeOuts.current]); // 배열에 요소가 있으면 componentDidMount와 componentDidUpdate를 둘 다 수행한다.
+//     // timeOuts.current가 바뀌는 시점은 새 배열을 넣어주는 시점 -> onClickRedo
 
 
-    const onClickRedo = () => {
-        setWinNumbers(getWinNumbers());
-        setWinBalls([]);
-        setBonus(null);
-        setRedo(false);
-        timeOuts.current = [];
-    }
-
-    const runTimeouts = () => {
-        for (let i = 0; i < winNumbers.length - 1; i++) {
-            timeOuts.current[i] = setTimeout(() => {
-                setWinBalls((prevWinBalls) => [...prevWinBalls, winNumbers[i]]);
-            }, (i + 1) * 1000);
-        }
-        timeOuts.current[6] = setTimeout(() => {
-            setBonus(winNumbers[6]);
-            setRedo(true);
-        }, 7000);
-    }
-
-    return (
-        <>
-            <div>당첨 숫자</div>
-            <div id="result">
-                {winBalls.map((v) => <Ball key={v} number={v} />)}
-            </div>
-            <div>보너스</div>
-            {bonus && <Ball number={bonus} />}
-            {redo && <button onClick={onClickRedo}>한번더!</button>}
-        </>
-    );
-}
-
-/* Class */
-// class Lotto extends Component {
-//     state = {
-//         winNumbers: getWinNumbers(),
-//         winBalls: [],
-//         bonus: null,
-//         redo: false,
+//     const onClickRedo = () => {
+//         setWinNumbers(getWinNumbers());
+//         setWinBalls([]);
+//         setBonus(null);
+//         setRedo(false);
+//         timeOuts.current = [];
 //     }
 
-//     timeouts = [];
-//     runTimeouts = () => {
-//         const { winNumbers } = this.state;
+//     const runTimeouts = () => {
 //         for (let i = 0; i < winNumbers.length - 1; i++) {
-//             this.timeouts[i] = setTimeout(() => {
-//                 this.setState((prevState) => {
-//                     return {
-//                         winBalls: [...prevState.winBalls, winNumbers[i]],
-//                     }
-//                 })
+//             timeOuts.current[i] = setTimeout(() => {
+//                 setWinBalls((prevWinBalls) => [...prevWinBalls, winNumbers[i]]);
 //             }, (i + 1) * 1000);
 //         }
-//         this.timeouts[6] = setTimeout(() => {
-//             this.setState({
-//                 bonus: winNumbers[6],
-//                 redo: true,
-//             })
+//         timeOuts.current[6] = setTimeout(() => {
+//             setBonus(winNumbers[6]);
+//             setRedo(true);
 //         }, 7000);
 //     }
 
-
-//     componentDidMount() {
-//         this.runTimeouts();
-//     }
-
-//     componentDidUpdate(prevProps, prevState) {
-//         if (this.state.winBalls.length === 0) {
-//             this.runTimeouts();
-//         }
-//     }
-
-//     componentWillUnmount() {
-//         this.timeouts.forEach((v) => {
-//             clearTimeout(v);
-//         })
-//     }
-
-//     onClickRedo = () => {
-//         this.setState({
-//             winNumbers: getWinNumbers(),
-//             winBalls: [],
-//             bonus: null,
-//             redo: false,
-//         })
-//         this.timeouts = [];
-//     }
-
-//     render() {
-//         const { winBalls, bonus, redo } = this.state;
-//         return (
-//             <>
-//                 <div>당첨 숫자</div>
-//                 <div id="result">
-//                     {winBalls.map((v) => <Ball key={v} number={v} />)}
-//                 </div>
-//                 <div>보너스</div>
-//                 {bonus && <Ball number={bonus} />}
-//                 {redo && <button onClick={this.onClickRedo}>한번더!</button>}
-//             </>
-//         );
-//     }
+//     return (
+//         <>
+//             <div>당첨 숫자</div>
+//             <div id="result">
+//                 {winBalls.map((v) => <Ball key={v} number={v} />)}
+//             </div>
+//             <div>보너스</div>
+//             {bonus && <Ball number={bonus} />}
+//             {redo && <button onClick={onClickRedo}>한번더!</button>}
+//         </>
+//     );
 // }
+
+/* Class */
+class Lotto extends Component {
+    state = {
+        winNumbers: getWinNumbers(),
+        winBalls: [],
+        bonus: null,
+        redo: false,
+    }
+
+    timeouts = [];
+    runTimeouts = () => {
+        const { winNumbers } = this.state;
+        for (let i = 0; i < winNumbers.length - 1; i++) {
+            this.timeouts[i] = setTimeout(() => {
+                this.setState((prevState) => {
+                    return {
+                        winBalls: [...prevState.winBalls, winNumbers[i]],
+                    }
+                })
+            }, (i + 1) * 1000);
+        }
+        this.timeouts[6] = setTimeout(() => {
+            this.setState({
+                bonus: winNumbers[6],
+                redo: true,
+            })
+        }, 7000);
+    }
+
+
+    componentDidMount() {
+        this.runTimeouts();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.winBalls.length === 0) {
+            this.runTimeouts();
+        }
+    }
+
+    componentWillUnmount() {
+        this.timeouts.forEach((v) => {
+            clearTimeout(v);
+        })
+    }
+
+    onClickRedo = () => {
+        this.setState({
+            winNumbers: getWinNumbers(),
+            winBalls: [],
+            bonus: null,
+            redo: false,
+        })
+        this.timeouts = [];
+    }
+
+    render() {
+        const { winBalls, bonus, redo } = this.state;
+        return (
+            <>
+                <div>당첨 숫자</div>
+                <div id="result">
+                    {winBalls.map((v) => <Ball key={v} number={v} />)}
+                </div>
+                <div>보너스</div>
+                {bonus && <Ball number={bonus} />}
+                {redo && <button onClick={this.onClickRedo}>한번더!</button>}
+            </>
+        );
+    }
+}
 
 export default Lotto;
--- rsp/RSP.jsx
+++ rsp/RSP.jsx
@@ -19,139 +19,139 @@ const cpuChoice = (imgCoord) => {
 }
 
 /* Hooks */
-const RSP = () => {
-    const [result, setResult] = useState('');
-    const [imgCoord, setImgCoord] = useState(rspCoords.바위);
-    const [score, setScore] = useState(0);
-    const interval = useRef();
+// const RSP = () => {
+//     const [result, setResult] = useState('');
+//     const [imgCoord, setImgCoord] = useState(rspCoords.바위);
+//     const [score, setScore] = useState(0);
+//     const interval = useRef();
 
-    useEffect(() => { // componentDidMount, componentDidUpdate 역할(1대 1 대응은 아님)
-        interval.current = setInterval(changeHand, 100);
-        return () => { // componentWillUnmount 역할
-            clearInterval(interval.current);
-        }
-    }, [imgCoord]); // 두 번째 인수 배열에 넣은 값들이 바뀔 때마다 useEffect가 실행된다.
+//     useEffect(() => { // componentDidMount, componentDidUpdate 역할(1대 1 대응은 아님)
+//         interval.current = setInterval(changeHand, 100);
+//         return () => { // componentWillUnmount 역할
+//             clearInterval(interval.current);
+//         }
+//     }, [imgCoord]); // 두 번째 인수 배열에 넣은 값들이 바뀔 때마다 useEffect가 실행된다.
 
-    const changeHand = () => {
-        if (imgCoord === rspCoords.바위) {
-            setImgCoord(rspCoords.가위);
-        } else if (imgCoord === rspCoords.가위) {
-            setImgCoord(rspCoords.보);
-        } else if (imgCoord === rspCoords.보) {
-            setImgCoord(rspCoords.바위);
-        }
-    }
-
-    const onClickBtn = (choice) => () => {
-        clearInterval(interval.current);
-        const myScore = scores[choice];
-        const cpuScore = scores[cpuChoice(imgCoord)];
-        const diff = myScore - cpuScore;
-        if (diff === 0) {
-            setResult('무승부');
-        } else if ([-1, 2].includes(diff)) {
-            setResult('승리');
-            setScore((prevScore) => prevScore + 1);
-        } else {
-            setResult('패배');
-            setScore((prevScore) => prevScore - 1);
-        }
-        setTimeout(() => {
-            interval.current = setInterval(changeHand, 100);
-        }, 3000);
-    }
-
-    return (
-        <>
-            <div id="computer" style={{ background: `url(https://miro.medium.com/max/4800/1*8du96SQUQ0NlWmWvVu20Zw.png) ${imgCoord} -150px` }}></div>
-            <div>
-                <button id="rock" className="btn" onClick={onClickBtn('바위')}>바위</button>
-                <button id="scissors" className="btn" onClick={onClickBtn('가위')}>가위</button>
-                <button id="paper" className="btn" onClick={onClickBtn('보')}>보</button>
-            </div>
-            <div>{result}</div>
-            <div>현재 {score}점</div>
-        </>
-    );
-}
-
-/* Class */
-// class RSP extends Component {
-//     state = {
-//         result: '',
-//         imgCoord: rspCoords.바위,
-//         score: 0,
-//     }
-
-//     interval;
-//     componentDidMount() {
-//         // const { imgCoord } = this.state; // 코드를 여기에 두면 클로저 문제 발생
-//         this.interval = setInterval(this.changeHand, 100);
-//     }
-
-//     changeHand = () => {
-//         const { imgCoord } = this.state;
+//     const changeHand = () => {
 //         if (imgCoord === rspCoords.바위) {
-//             this.setState({
-//                 imgCoord: rspCoords.가위,
-//             })
+//             setImgCoord(rspCoords.가위);
 //         } else if (imgCoord === rspCoords.가위) {
-//             this.setState({
-//                 imgCoord: rspCoords.보,
-//             })
+//             setImgCoord(rspCoords.보);
 //         } else if (imgCoord === rspCoords.보) {
-//             this.setState({
-//                 imgCoord: rspCoords.바위,
-//             })
+//             setImgCoord(rspCoords.바위);
 //         }
 //     }
 
-//     onClickBtn = (choice) => () => {
-//         const { imgCoord } = this.state;
-
-//         clearInterval(this.interval);
+//     const onClickBtn = (choice) => () => {
+//         clearInterval(interval.current);
 //         const myScore = scores[choice];
 //         const cpuScore = scores[cpuChoice(imgCoord)];
 //         const diff = myScore - cpuScore;
 //         if (diff === 0) {
-//             this.setState({
-//                 result: '무승부',
-//             })
+//             setResult('무승부');
 //         } else if ([-1, 2].includes(diff)) {
-//             this.setState((prevState) => {
-//                 return {
-//                     result: '승리',
-//                     score: prevState.score + 1,
-//                 }
-//             })
+//             setResult('승리');
+//             setScore((prevScore) => prevScore + 1);
 //         } else {
-//             this.setState((prevState) => {
-//                 return {
-//                     result: '패배',
-//                     score: prevState.score - 1,
-//                 }
-//             })
+//             setResult('패배');
+//             setScore((prevScore) => prevScore - 1);
 //         }
 //         setTimeout(() => {
-//             this.interval = setInterval(this.changeHand, 100);
+//             interval.current = setInterval(changeHand, 100);
 //         }, 3000);
 //     }
 
-//     render() {
-//         const { result, imgCoord, score } = this.state;
-//         return (
-//             <>
-//                 <div id="computer" style={{ background: `url(https://miro.medium.com/max/4800/1*8du96SQUQ0NlWmWvVu20Zw.png) ${imgCoord} -150px` }}></div>
-//                 <div>
-//                     <button id="rock" className="btn" onClick={this.onClickBtn('바위')}>바위</button>
-//                     <button id="scissors" className="btn" onClick={this.onClickBtn('가위')}>가위</button>
-//                     <button id="paper" className="btn" onClick={this.onClickBtn('보')}>보</button>
-//                 </div>
-//                 <div>{result}</div>
-//                 <div>현재 {score}점</div>
-//             </>
-//         );
-//     }
+//     return (
+//         <>
+//             <div id="computer" style={{ background: `url(https://miro.medium.com/max/4800/1*8du96SQUQ0NlWmWvVu20Zw.png) ${imgCoord} -150px` }}></div>
+//             <div>
+//                 <button id="rock" className="btn" onClick={onClickBtn('바위')}>바위</button>
+//                 <button id="scissors" className="btn" onClick={onClickBtn('가위')}>가위</button>
+//                 <button id="paper" className="btn" onClick={onClickBtn('보')}>보</button>
+//             </div>
+//             <div>{result}</div>
+//             <div>현재 {score}점</div>
+//         </>
+//     );
 // }
+
+/* Class */
+class RSP extends Component {
+    state = {
+        result: '',
+        imgCoord: rspCoords.바위,
+        score: 0,
+    }
+
+    interval;
+    componentDidMount() {
+        // const { imgCoord } = this.state; // 코드를 여기에 두면 클로저 문제 발생
+        this.interval = setInterval(this.changeHand, 100);
+    }
+
+    changeHand = () => {
+        const { imgCoord } = this.state;
+        if (imgCoord === rspCoords.바위) {
+            this.setState({
+                imgCoord: rspCoords.가위,
+            })
+        } else if (imgCoord === rspCoords.가위) {
+            this.setState({
+                imgCoord: rspCoords.보,
+            })
+        } else if (imgCoord === rspCoords.보) {
+            this.setState({
+                imgCoord: rspCoords.바위,
+            })
+        }
+    }
+
+    onClickBtn = (choice) => () => {
+        const { imgCoord } = this.state;
+
+        clearInterval(this.interval);
+        const myScore = scores[choice];
+        const cpuScore = scores[cpuChoice(imgCoord)];
+        const diff = myScore - cpuScore;
+        if (diff === 0) {
+            this.setState({
+                result: '무승부',
+            })
+        } else if ([-1, 2].includes(diff)) {
+            this.setState((prevState) => {
+                return {
+                    result: '승리',
+                    score: prevState.score + 1,
+                }
+            })
+        } else {
+            this.setState((prevState) => {
+                return {
+                    result: '패배',
+                    score: prevState.score - 1,
+                }
+            })
+        }
+        setTimeout(() => {
+            this.interval = setInterval(this.changeHand, 100);
+        }, 3000);
+    }
+
+    render() {
+        const { result, imgCoord, score } = this.state;
+        return (
+            <>
+                <div id="computer" style={{ background: `url(https://miro.medium.com/max/4800/1*8du96SQUQ0NlWmWvVu20Zw.png) ${imgCoord} -150px` }}></div>
+                <div>
+                    <button id="rock" className="btn" onClick={this.onClickBtn('바위')}>바위</button>
+                    <button id="scissors" className="btn" onClick={this.onClickBtn('가위')}>가위</button>
+                    <button id="paper" className="btn" onClick={this.onClickBtn('보')}>보</button>
+                </div>
+                <div>{result}</div>
+                <div>현재 {score}점</div>
+            </>
+        );
+    }
+}
 
 export default RSP;
